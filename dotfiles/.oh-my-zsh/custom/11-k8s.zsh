594f102eac8e61bc2ad661478039e621342cd27e
Jim Weller <jim.weller@gmail.com>
2025-05-02 09:30:19 -0700

    1746203419

--- dotfiles/.oh-my-zsh/custom/11-k8s.zsh
+++ dotfiles/.oh-my-zsh/custom/11-k8s.zsh
@@ -9,3 +9,19 @@ alias kctx="kubectx"
 alias upgrade="bubo && bugbc"
 
 alias hxns='k config set-context --current --namespace'
+
+
+rancher_kubeconfigs() {
+  rancher_clusters=$(mktemp)
+  final_config=$(mktemp)
+
+  rancher clusters | awk 'NR>1 {print $2}' | while read -r cluster; do
+    rancher kubeconfig "$cluster" >> "$rancher_clusters"
+  done
+
+  KUBECONFIG=~/.kube/config:$rancher_clusters kubectl config view --flatten > "$final_config"
+  mv "$final_config" ~/.kube/config
+  chmod 600 ~/.kube/config
+
+  rm "$rancher_clusters"
+}
