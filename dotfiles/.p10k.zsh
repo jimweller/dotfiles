070717a5c3fc6ce3631f0b7ccfa12eb257025e5d
Jim Weller <jim.weller@gmail.com>
2025-05-03 22:53:58 -0700

    1746338038

--- dotfiles/.p10k.zsh
+++ dotfiles/.p10k.zsh
@@ -1792,7 +1792,7 @@ function prompt_gituser() {
   local alias=${GIT_USERNAME_ALIASES[$GIT_USERNAME]:-$GIT_USERNAME}
   [[ -n $alias ]] || return
 
-  _p9k_prompt_segment "$0" $_p9k_color1 $POWERLEVEL9K_GIT_USER_COLOR POWERLEVEL9K_GIT_USER_ICON  0 '' $alias
+  _p9k_prompt_segment "$0" $_p9k_color1 $POWERLEVEL9K_GIT_USER_COLOR GIT_USER_ICON 0 '' $alias
 }
 
 
