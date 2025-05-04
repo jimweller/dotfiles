278978be7b8c07fa13124d58adc37f0436fdcdab
Jim Weller <jim.weller@gmail.com>
2025-05-03 21:43:48 -0700

    1746333828

--- dotfiles/.p10k.zsh
+++ dotfiles/.p10k.zsh
@@ -34,7 +34,7 @@
     # =========================[ Line #1 ]=========================
     os_icon                 # os identifier
     dir                     # current directory
-    vcs                     # git status
+    gitstatus                     # git status
     # =========================[ Line #2 ]=========================
     newline                 # \n
     prompt_char             # prompt symbol
