a559ee4a060f99f6a18df35ce696dc23b7707ae8
Jim Weller <jim.weller@gmail.com>
2025-05-03 18:59:21 -0700

    1746323961

--- dotfiles/.p10k.zsh
+++ dotfiles/.p10k.zsh
@@ -169,7 +169,7 @@
   typeset -g POWERLEVEL9K_BACKGROUND=234
 
   # Separator between same-color segments on the left.
-  typeset -g POWERLEVEL9K_LEFT_SUBSEGMENT_SEPARATOR=''
+  typeset -g POWERLEVEL9K_LEFT_SUBSEGMENT_SEPARATOR='%242F\uE0B1'
   # Separator between same-color segments on the right.
   typeset -g POWERLEVEL9K_RIGHT_SUBSEGMENT_SEPARATOR='%242F\uE0B3'
   # Separator between different-color segments on the left.
