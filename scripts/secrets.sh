1a8d9f6c0f9f294ef4757a223c876b33c431854f
Jim Weller <jim.weller@gmail.com>
2025-05-20 10:35:43 -0700

    1747762543

--- scripts/secrets.sh
+++ scripts/secrets.sh
@@ -14,8 +14,8 @@ fi
 
 TMPFILE=$(mktemp)
 
-if [[ "$MODE" != "open" && "$MODE" != "save" ]]; then
-  echo "Usage: $0 [open|save] [password] [archive_file]"
+if [[ "$MODE" != "open" && "$MODE" != "save" && "$MODE" != "list" ]]; then
+  echo "Usage: $0 [open|save|list] [password] [archive_file]"
   exit 1
 fi
 
@@ -54,4 +54,18 @@ elif [ "$MODE" = "save" ]; then
   tar --disable-copyfile --no-xattrs -cvzf - \
     -C "$HOME" $(echo "$FILES" | sed "s|$HOME/||g") \
     | gpg --batch --yes --passphrase "$PASSWORD" --symmetric --cipher-algo AES256 -o "$ARCHIVE"
+    
+elif [ "$MODE" = "list" ]; then
+  while true; do
+    if echo "$PASSWORD" | gpg --decrypt --pinentry-mode loopback --passphrase-fd 0 "$ARCHIVE" > "$TMPFILE" ; then
+      echo "Contents of encrypted archive:"
+      tar -tzf "$TMPFILE"
+      rm -f "$TMPFILE"
+      break
+    fi
+    echo "Incorrect passphrase, try again."
+    printf "Enter passphrase: "
+    read -s PASSWORD
+    echo
+  done
 fi
