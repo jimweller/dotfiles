255796fdf36a7d69353548047b6c32ef31c1ab04
Jim Weller <jim.weller@gmail.com>
2025-08-10 14:26:12 -0700

    1754861172

--- dotfiles/.oh-my-zsh/custom/01-path.zsh
+++ dotfiles/.oh-my-zsh/custom/01-path.zsh
@@ -45,7 +45,10 @@ if [ -d "$HOME/.local/bin" ]; then
   export PATH="$HOME/.local/bin:$PATH"
 fi
 
-
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+mkdir -p "$HOME/.go"
+export GOPATH="$HOME/.go"
+export PATH="$GOPATH/bin:$PATH"
