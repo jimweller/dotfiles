9bc630af7df58a563fef20597231278f8984d999
Jim Weller <jim.weller@gmail.com>
2025-08-15 16:21:37 -0700

    1755300097

--- dotfiles/oh-my-zsh/uv/uv.plugin.zsh
+++ dotfiles/oh-my-zsh/uv/uv.plugin.zsh
@@ -20,3 +20,21 @@ alias uvsu='uv sync --upgrade'
 alias uvup='uv self update'
 alias uvv='uv venv'
 
+# If the completion file doesn't exist yet, we need to autoload it and
+# bind it. Otherwise, compinit will have already done that.
+if [[ ! -f "$ZSH_CACHE_DIR/completions/_uv" ]]; then
+  typeset -g -A _comps
+  autoload -Uz _uv
+  _comps[uv]=_uv
+fi
+
+if [[ ! -f "$ZSH_CACHE_DIR/completions/_uvx" ]]; then
+  typeset -g -A _comps
+  autoload -Uz _uvx
+  _comps[uvx]=_uvx
+fi
+
+# uv and uvx are installed together (uvx is an alias to `uv tool run`)  
+# Overwrites the file each time as completions might change with uv versions.
+uv generate-shell-completion zsh >| "$ZSH_CACHE_DIR/completions/_uv" &|
+uvx --generate-shell-completion zsh >| "$ZSH_CACHE_DIR/completions/_uvx" &|
