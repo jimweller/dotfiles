4f67545441d8f07fdd3bf35914dd1cfe27c4e952
Jim Weller <jim.weller@gmail.com>
2025-09-03 09:16:36 -0700

    1756916196

---
+++ dotfiles/zsh-jim/alehouse/alehouse.plugin.zsh
@@ -0,0 +1,81 @@
+alias br="brew"
+alias brana='br analytics'
+alias brcat='br cat'
+alias brcle='br cleanup'
+alias brcom='br command'
+alias brcoms='br commands'
+alias brcon='br config'
+alias brdep='br deps'
+alias brdes='br desc'
+alias brdiy='br diy'
+alias brdoc='br doctor'
+alias brfet='br fetch'
+alias brgis='br gist-logs'
+alias brhom='br home'
+alias brinf='br info'
+alias brins='br install'
+alias brlea='br leaves'
+alias brlis='br list'
+alias brln='br ln'
+alias brlog='br log'
+alias brmig='br migrate'
+alias brmig='br missing'
+alias broca='br --cache'
+alias broce='br --cellar'
+alias broen='br --env'
+alias bropr='br --prefix'
+alias bropt='br options'
+alias brore='br --repository'
+alias brout='br outdated'
+alias brove='br --version'
+alias brpin='br pin'
+alias brpos='br postinstall'
+# alias brpru='br prune'
+alias brrea='br readall'
+alias brrei='br reinstall'
+alias brsea='br search'
+alias brsh='br sh'
+alias brsty='br style'
+alias brswi='br switch'
+alias brtap='br tap'
+alias brtapi='br tap-info'
+alias brtapp='br tap-pin'
+alias brtapu='br tap-unpin'
+alias bruin='br uninstall'
+alias bruli='br unlink'
+alias brupa='br unpack'
+alias brupd='br update'
+alias brupdr='br update-reset'
+alias brupg='br upgrade'
+alias brupi='br unpin'
+alias bruse='br uses'
+alias bruta='br untap'
+alias brse='br services'
+alias brseli='brse list'
+alias sbrse='sudo brse'
+
+# Matt's additions
+alias brewup="brew -v update && brew -v upgrade && brew upgrade --cask && brew -v cleanup --prune=5 && brew doctor"
+alias bi='brew info' # conflicts with bundler! comment this out to use that alias instead
+alias bis='brew install'
+alias ci='brew info --cask'
+alias cis='brew install --cask'
+alias bs="brew search"
+alias bsd="brew search --desc"
+alias cus="brew uninstall --cask"
+alias brdr="brew doctor"
+alias cuz="brew zap --cask"
+alias blv='brew leaves'
+alias brls='brew ls'
+
+alias findappcast="$(brew --repository)/Library/Taps/homebrew/homebrew-cask/developer/bin/find_appcast"
+alias tokenizer="$(brew --repository)/Library/Taps/Homebrew/homebrew-cask/developer/bin/generate_cask_token"
+
+# hashes for easier cd'ing
+# example: cd ~taps
+hash -d brewr="$(brew --repo)"
+hash -d brpfx="$(brew --prefix)"
+hash -d taps="$(brew --prefix)/Homebrew/Library/Taps/"
+hash -d cellar="$(brew --cellar)/"
+hash -d caskroom="$(brew --prefix)/Caskroom/"
+hash -d brche="$(brew --cache)"
