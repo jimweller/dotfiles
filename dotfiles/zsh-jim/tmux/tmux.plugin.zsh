db1d0653fbbc3db0d861696812e5d1d9482463c8
Jim Weller <jim.weller@gmail.com>
2025-11-15 11:34:15 -0800

    tmux dont autostart

--- dotfiles/zsh-jim/tmux/tmux.plugin.zsh
+++ dotfiles/zsh-jim/tmux/tmux.plugin.zsh
@@ -16,12 +16,12 @@ alias tkss='tmux kill-session -t'
 
 # CONFIGURATION VARIABLES
 # Automatically start tmux
-: ${ZSH_TMUX_AUTOSTART:=true}
+: ${ZSH_TMUX_AUTOSTART:=false}
 # Only autostart once. If set to false, tmux will attempt to
 # autostart every time your zsh configs are reloaded.
 : ${ZSH_TMUX_AUTOSTART_ONCE:=true}
 # Automatically connect to a previous session if it exists
-: ${ZSH_TMUX_AUTOCONNECT:=false}
+: ${ZSH_TMUX_AUTOCONNECT:=true}
 # Automatically close the terminal when tmux exits
 : ${ZSH_TMUX_AUTOQUIT:=$ZSH_TMUX_AUTOSTART}
 # Set term to screen or screen-256color based on current terminal support
