b04d46dd937b038378b08bce92d9c592bfea4ca5
Jim Weller <jim.weller@gmail.com>
2026-06-30 14:26:04 -0700

    1782854764

--- configs/zsh-jim/00-secrets.zsh
+++ configs/zsh-jim/00-secrets.zsh
@@ -1,7 +1,9 @@
 if [[ -d "$HOME/.secrets" ]]; then
+  set -a
   for secret_file in "$HOME/.secrets"/*.env; do
     if [[ -f "$secret_file" ]]; then
       source $secret_file
     fi
   done
+  set +a
 fi
